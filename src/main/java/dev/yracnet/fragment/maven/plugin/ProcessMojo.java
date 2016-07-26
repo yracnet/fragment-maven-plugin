5ae270e9b6612e8ff6886fe74f30424574cdbfb3
Willyams Yujra <yracnet@gmail.com>
2016-07-26 17:38:14 -0400

    PluginExecution PublingFragment

--- src/main/java/dev/yracnet/fragment/maven/plugin/ProcessMojo.java
+++ src/main/java/dev/yracnet/fragment/maven/plugin/ProcessMojo.java
@@ -1,28 +1,48 @@
 package dev.yracnet.fragment.maven.plugin;
 
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.BuildPluginManager;
+import dev.yracnet.fragment.maven.plugin.data.PluginExecution;
+import dev.yracnet.fragment.maven.plugin.data.PluginFragment;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.twdata.maven.mojoexecutor.MojoExecutor;
 
 @Mojo(name = "process", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
-public class ProcessMojo extends AbstractMojo {
+public class ProcessMojo extends ContextMojo {
 
-	@Component
-	private MavenProject mavenProject;
-
-	@Component
-	private MavenSession mavenSession;
-
-	@Component
-	private BuildPluginManager pluginManager;
-
-	@Parameter(alias="fragments")
+	/**
+	 * XML Fragment of Plugin Maven
+	 * <pre>{@code
+	 *  <fragments>
+	 *   <fragment>formatter.xml</fragment>
+	 *  </fragments>
+	 * }</pre>
+	 *
+	 * File: formatter.xml
+	 * <pre>{@code
+	 * <?xml version="1.0" encoding="UTF-8"?>
+	 * <plugin>
+	 *  <groupId>net.revelc.code</groupId>
+	 *  <artifactId>formatter-maven-plugin</artifactId>
+	 *  <version>0.5.2</version>
+	 *  <executions>
+	 *   <execution>
+	 *    <goals>
+	 *     <goal>format</goal>
+	 *    </goals>
+	 *   </execution>
+	 *  </executions>
+	 * <configuration>
+	 *  <lineEnding>CRLF</lineEnding>
+	 *  <encoding>UTF-8</encoding>
+	 * </configuration>
+	 * </plugin>
+	 * }</pre>
+	 */
+	@Parameter(alias = "fragments")
 	private String[] fragment;
 
 	public void space() {
@@ -33,14 +53,41 @@ public class ProcessMojo extends AbstractMojo {
 	public void execute() throws MojoExecutionException {
 		space();
 		if (fragment != null && fragment.length > 0) {
-			executeFragment();
+			for (String name : fragment) {
+				execute(name);
+			}
 		}
 		space();
 	}
 
-	private void executeFragment() {
-		for (String name : fragment) {
-			getLog().info("PROCESS FRAGMENT " + name);
+	public void execute(String name) throws MojoExecutionException {
+		PluginFragment pluginFragment = getPluginFragment(name);
+		if (pluginFragment != null && pluginFragment.isSkip() == false) {
+			execute(pluginFragment);
+		}
+	}
+
+	public void execute(PluginFragment pluginFragment) throws MojoExecutionException {
+		Plugin plugin = pluginFragment.getPlugin();
+		PluginExecution executionArray[] = pluginFragment.getExecution();
+		for (PluginExecution execution : executionArray) {
+			execute(plugin, execution, pluginFragment.getConfiguration());
+		}
+	}
+
+	public void execute(Plugin plugin, PluginExecution execution, Xpp3Dom configuration) throws MojoExecutionException {
+		String[] goalArray = execution.getGoal();
+		MojoExecutor.ExecutionEnvironment env = getCurrentExecutionEnvironment();
+		for (String goalName : goalArray) {
+			getLog().info("PROCESS FRAGMENT " + plugin);
+			getLog().info("GOAL NAME: " + goalName);
+			getLog().debug("PROCESS FRAGMENT " + plugin + " - " + configuration);
+			MojoExecutor.executeMojo(
+											plugin,
+											goalName,
+											configuration,
+											env
+			);
 		}
 	}
 }
